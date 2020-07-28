2903d3040416ee74ab9a93b0ed08c6776060b3a9
vinesh <vineshghela@gmail.com>
2020-07-28 11:17:18 +0100

    gone

--- src/main/java/com/QA/respository/NotesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.QA.respository;
-
-import com.QA.models.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-    public interface NotesRepository extends JpaRepository<Note, Long> {
-    }
-
